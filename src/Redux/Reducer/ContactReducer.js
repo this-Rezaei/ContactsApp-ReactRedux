81842ea1f2e5485f9795a6d44bc72002e5b9e5ec
Ali Rezaei <ali.rezaei0021@gmail.com>
2021-08-24 16:58:17 +0430

    send data

--- src/Redux/Reducer/ContactReducer.js
+++ src/Redux/Reducer/ContactReducer.js
@@ -2,17 +2,23 @@ const initialState = [
     {
         id: 0,
         name: "علی رضایی",
+        email: "",
         number: 9338430259,
     },
     {
         id: 0,
         name: "امید سرایی",
+        email: "",
         number: 9395468546,
     },
 ];
 
 const ContactReducer = (state = initialState, action) => {
     switch (action.type) {
+
+      case "ADD_CONTACT":
+         return [...state,action.paylod]
+      break;
         default:
             return state;
             break;
--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,19 +1,43 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 const AddContact = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [number, setNumber] = useState("");
     const [valid, setvalid] = useState("");
-    console.log(valid);
+    const dispatch = useDispatch();
+    const History = useHistory();
     const contacts = useSelector((contact) => contact);
     const SubmitHandler = (e) => {
         e.preventDefault();
+        const checkEmail = contacts.find(
+            (contact) => contact.email === email && contact
+        );
+        const checkNumber = contacts.find(
+            (contact) => contact.number === parseInt(number)
+        );
+
         if (!email || !name || !number) {
             return toast.warning("فیلد ها خالی هستند");
         }
+        if (checkEmail) {
+            return toast.error("ایمیل صحیح نمی باشد");
+        }
+        if (checkNumber) {
+            return toast.error("شماره صحیح نمی باشد");
+        }
+        const data = {
+            id: contacts[contacts.length - 1].id + 1,
+            name,
+            email,
+            number,
+        };
+        dispatch({ type: "ADD_CONTACT", paylod: data });
+        toast.success("مخاطب با موفقیت ذخیره شد");
+        History.push("/");
     };
     const clickHandler = () => {
         if (name.length == 0) {
